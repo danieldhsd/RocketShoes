ea3b269c41096c599635905368ad166a455556a8
Daniel Dutra <danieldhsd@gmail.com>
2019-12-23 20:06:30 -0300

    Estilos globais e criando headers

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
+import GlobalStyle from './styles/global';
 import Routes from './routes';
+import Header from './components/Header';
 
 function App() {
   return (
     <BrowserRouter>
+      <Header></Header>
       <Routes></Routes>
+      <GlobalStyle></GlobalStyle>
     </BrowserRouter>
   );
 }
